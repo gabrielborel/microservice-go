ac156a0ec40b42052b55aefc264410148be41800
Gabriel Borel <biel_borel@hotmail.com>
2023-04-30 17:35:53 -0300

    feat: order repo created

---
+++ internal/order/infra/database/order_repository.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/gabrielborel/microservice-go/internal/order/entity"
+	_ "github.com/lib/pq"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{Db: db}
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	stmt, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES ($1, $2, $3, $4)")
+	if err != nil {
+		panic(err)
+	}
+	_, err = stmt.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+	return nil
+}
