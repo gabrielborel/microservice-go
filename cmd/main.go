fcaa95a680d577cce2bea6225e8184dd4097de11
Gabriel Borel <biel_borel@hotmail.com>
2023-04-30 23:47:43 -0300

    feat: rabbitmq pkg, and some testing, very cool

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"sync"
 
 	"github.com/gabrielborel/microservice-go/internal/order/infra/database"
 	"github.com/gabrielborel/microservice-go/internal/order/usecases"
+	"github.com/gabrielborel/microservice-go/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -20,12 +25,43 @@ func main() {
 
 	repository := database.NewOrderRepository(db)
 	usecase := usecases.NewCalculateFinalPriceUseCase(repository)
-	_, err = usecase.Execute(usecases.OrderInputDTO{
-		ID:    "1234567",
-		Price: 100.0,
-		Tax:   15.0,
-	})
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
+	}
+	defer ch.Close()
+
+	maxWorkers := 20
+	wg := sync.WaitGroup{}
+
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out)
+
+	wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		defer wg.Done()
+		go worker(out, usecase, i)
+	}
+	wg.Wait()
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, usecase *usecases.CalculateFinalPriceUseCase, workerId int) {
+	for msg := range deliveryMessage {
+		var input usecases.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			fmt.Println("Error while unmarshalling message", err)
+		}
+
+		input.Tax = 10.0
+
+		fmt.Println("Input DTO", input)
+		_, err = usecase.Execute(input)
+		if err != nil {
+			panic(err)
+		}
+
+		println("Received a message from worker: %s", workerId)
+		msg.Ack(false)
 	}
 }
